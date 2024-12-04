55226993bd8fa780740a8ca42afae6dc04c18b7c
xueth <xuethlovelicy@gmail.com>
2024-12-04 17:45:44 +0800

    Methods.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,23 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -14,10 +31,18 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect2 = Rectangle {
+        width: 30,
+        height: 45,
+    };
+    println!("rect2 is {rect2:?}");
+
+    println!("rect1 can hold rect2? {}", rect1.can_hold(&rect2));
+
+    // Associated function
+    let sq = Rectangle::square(5);
+    println!("sq is {sq:?}");
 }
